55abe4ebeb8e73e135394e4da6906872325fefe3
Maxwell Crawford <crawfordmaxwell24@gmail.com>
2020-01-29 12:07:58 -0500

    refactor: location_filters r2; var renames

--- filters/location_filters.py
+++ filters/location_filters.py
@@ -7,9 +7,9 @@ def effect_random_pixel_shift(img, offset_range=100, patchx=0, patchy=0, patches
     Leave patchx, patchy at 0 to randomize!
     :param img:
     :param offset_range: max amount it can be shifted
-    :return newimg:
+    :return new_img:
     """
-    newimg = np.copy(img)
+    new_img = np.copy(img)
     height = img.shape[0]  # j, patchy
     width = img.shape[1]  # i, patchx
     # current_offset = 0
@@ -40,8 +40,8 @@ def effect_random_pixel_shift(img, offset_range=100, patchx=0, patchy=0, patches
         for i in range(istart, istart + patchx):  # NOTE the usage of 1,w-1; 1,h-1
             for j in range(jstart, jstart + patchy):
                 # Determine if we will shift:
-                doShift = random.randrange(0, 3)
-                if doShift == 0:
+                do_shift = random.randrange(0, 3)
+                if do_shift == 0:
                     # Determine offset amt and location
                     current_offset = random.randrange(-1 * offset_range, offset_range)
                     offset_where = random.randrange(0, 3)
@@ -49,21 +49,21 @@ def effect_random_pixel_shift(img, offset_range=100, patchx=0, patchy=0, patches
                     current_offset_abs = abs(current_offset)
                     if offset_where == 0:  # x
                         if (current_offset_abs + j) < height:
-                            newimg[j, i, 0] = img[j + current_offset, i, 0]
-                            newimg[j, i, 1] = img[j + current_offset, i, 1]
-                            newimg[j, i, 2] = img[j + current_offset, i, 2]
+                            new_img[j, i, 0] = img[j + current_offset, i, 0]
+                            new_img[j, i, 1] = img[j + current_offset, i, 1]
+                            new_img[j, i, 2] = img[j + current_offset, i, 2]
                     elif offset_where == 1:  # y
                         if (current_offset_abs + i) < width:
-                            newimg[j, i, 0] = img[j, i + current_offset, 0]
-                            newimg[j, i, 1] = img[j, i + current_offset, 1]
-                            newimg[j, i, 2] = img[j, i + current_offset, 2]
+                            new_img[j, i, 0] = img[j, i + current_offset, 0]
+                            new_img[j, i, 1] = img[j, i + current_offset, 1]
+                            new_img[j, i, 2] = img[j, i + current_offset, 2]
                     elif offset_where == 2:  # x, y
                         if (current_offset_abs + j) < height:
                             if (current_offset_abs + i) < width:
-                                newimg[j, i, 0] = img[j + current_offset, i + current_offset, 0]
-                                newimg[j, i, 1] = img[j + current_offset, i + current_offset, 1]
-                                newimg[j, i, 2] = img[j + current_offset, i + current_offset, 2]
-    return newimg
+                                new_img[j, i, 0] = img[j + current_offset, i + current_offset, 0]
+                                new_img[j, i, 1] = img[j + current_offset, i + current_offset, 1]
+                                new_img[j, i, 2] = img[j + current_offset, i + current_offset, 2]
+    return new_img
 
 
 def effect_horiz_shift(img):
@@ -72,11 +72,11 @@ def effect_horiz_shift(img):
     take entire selected row patch (y1 to y2) left;
     Increase offset each shift, but if overflow-> kick over to right side (width-current)
     :param img:
-    :return newimg:
+    :return new_img:
     """
     height = img.shape[0]  # j, patchy
     width = img.shape[1]  # i, patchx
-    newimg = np.copy(img)
+    new_img = np.copy(img)
     patches = random.randrange(3, 7)
     low = 1.07
     med = 1.10
@@ -94,39 +94,39 @@ def effect_horiz_shift(img):
             patchend = height - 1
 
         # Randomize color channel to focus on:
-        colorfocus = random.randrange(0, 4)
+        color_focus = random.randrange(0, 4)
 
         # Get divisor amount:
-        lowmedhigh = random.randrange(0, 3)
-        if lowmedhigh == 0:
+        divisor_selector = random.randrange(0, 3)
+        if divisor_selector == 0:
             divisor = low
-        elif lowmedhigh == 1:
+        elif divisor_selector == 1:
             divisor = med
-        elif lowmedhigh == 2:
+        elif divisor_selector == 2:
             divisor = high
 
         # Loop thru patch window
         for i in range(width):
             for j in range(patchy, patchend):
                 # Get actual shift amount, and determine if overflow:
-                shiftresult = i - shift
-                if shiftresult < 0:
-                    shiftresult = width + shiftresult
-                if colorfocus == 0:
-                    newimg[j, shiftresult, 0] = img[j, i, 0]
-                    newimg[j, shiftresult, 1] = int(img[j, i, 1] // divisor)
-                    newimg[j, shiftresult, 2] = int(img[j, i, 2] // divisor)
-                elif colorfocus == 1:
-                    newimg[j, shiftresult, 0] = int(img[j, i, 0] // divisor)
-                    newimg[j, shiftresult, 1] = img[j, i, 1]
-                    newimg[j, shiftresult, 2] = int(img[j, i, 2] // divisor)
-                elif colorfocus == 2:
-                    newimg[j, shiftresult, 0] = int(img[j, i, 0] // divisor)
-                    newimg[j, shiftresult, 1] = int(img[j, i, 1] // divisor)
-                    newimg[j, shiftresult, 2] = img[j, i, 2]
+                shift_result = i - shift
+                if shift_result < 0:
+                    shift_result = width + shift_result
+                if color_focus == 0:
+                    new_img[j, shift_result, 0] = img[j, i, 0]
+                    new_img[j, shift_result, 1] = int(img[j, i, 1] // divisor)
+                    new_img[j, shift_result, 2] = int(img[j, i, 2] // divisor)
+                elif color_focus == 1:
+                    new_img[j, shift_result, 0] = int(img[j, i, 0] // divisor)
+                    new_img[j, shift_result, 1] = img[j, i, 1]
+                    new_img[j, shift_result, 2] = int(img[j, i, 2] // divisor)
+                elif color_focus == 2:
+                    new_img[j, shift_result, 0] = int(img[j, i, 0] // divisor)
+                    new_img[j, shift_result, 1] = int(img[j, i, 1] // divisor)
+                    new_img[j, shift_result, 2] = img[j, i, 2]
                 else:
                     # ORIG w/o color band
-                    newimg[j, shiftresult, 0] = img[j, i, 0]
-                    newimg[j, shiftresult, 1] = img[j, i, 1]
-                    newimg[j, shiftresult, 2] = img[j, i, 2]
-    return newimg
+                    new_img[j, shift_result, 0] = img[j, i, 0]
+                    new_img[j, shift_result, 1] = img[j, i, 1]
+                    new_img[j, shift_result, 2] = img[j, i, 2]
+    return new_img
